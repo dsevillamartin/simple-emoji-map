ffe0ff2307fa5db3ee68e975dd420293cf7651fc
David Wheatley <davidwheatley03@gmail.com>
2020-06-29 20:55:41 +0100

    Lint config.js

--- src/config.js
+++ src/config.js
@@ -2,17 +2,14 @@ const cosmiconfig = require('cosmiconfig');
 const { name } = require('../package.json');
 
 const explorer = cosmiconfig(name, {
-  searchPlaces: [
-      ".simple-emoji-map",
-      ".simple-emoji-map.json",
-    ],
+    searchPlaces: ['.simple-emoji-map', '.simple-emoji-map.json'],
 });
 const def = {
-  shortnames: {},
-  type: 'emoji',
-  regex: null,
-}
+    shortnames: {},
+    type: 'emoji',
+    regex: null,
+};
 const defaults = (obj, defs) => Object.assign({}, obj, defs, obj);
 
-
-module.exports = () => explorer.search().then(e => defaults(e && e.config, def));
+module.exports = () =>
+    explorer.search().then((e) => defaults(e && e.config, def));
